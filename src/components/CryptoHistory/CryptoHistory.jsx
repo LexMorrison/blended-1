a6724bb13c41c7715d75c45d1ab8a79ef9c9f408
Tanyusha7 <swanonlake@gmail.com>
2023-06-24 14:37:36 +0200

    add task-4

--- src/components/CryptoHistory/CryptoHistory.jsx
+++ src/components/CryptoHistory/CryptoHistory.jsx
@@ -1,5 +1,45 @@
+import PropTypes from 'prop-types';
+import { formatDateToNow } from 'helpers/formatDateToNow';
 import { BaseTable, THead, Th, Tr, Td } from './CryptoHistory.styled';
 
-export const CryptoHistory = () => {
-  return <div>CryptoHistory</div>;
+export const CryptoHistory = ({ items }) => {
+  return (
+    <>
+      <BaseTable>
+        <THead>
+          <tr>
+            <Th>№</Th>
+            <Th>PRICE</Th>
+            <Th>AMOUNT</Th>
+            <Th>DATE</Th>
+          </tr>
+        </THead>
+
+        <tbody>
+          {items &&
+            items.map(({ id, price, amount, date }, index) => {
+              return (
+                <Tr key={id}>
+                  <Td>{index}</Td>
+                  <Td>{price}</Td>
+                  <Td>{amount}</Td>
+                  <Td>{formatDateToNow(date)}</Td>
+                </Tr>
+              );
+            })}
+        </tbody>
+      </BaseTable>
+    </>
+  );
+};
+
+CryptoHistory.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      price: PropTypes.number.isRequired,
+      amount: PropTypes.number.isRequired,
+      date: PropTypes.string.isRequired,
+    }),
+  ),
 };
